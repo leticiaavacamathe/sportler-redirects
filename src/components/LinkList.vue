6f7857acdb207f9a62dd931d0b70045b82152e49
Leticia Avaca Mathe <leticia_a.m@hotmail.com>
2023-08-07 11:23:39 +0200

    Modify List to update automatically after any change

--- src/components/LinkList.vue
+++ src/components/LinkList.vue
@@ -1,50 +1,42 @@
 <script setup>
 import { db } from '../firebase.js'
-import { getDocs, collection, deleteDoc, doc } from 'firebase/firestore'
+import { collection, doc, deleteDoc, onSnapshot } from 'firebase/firestore'
 import { ref, onMounted } from 'vue'
 import AddOrUpdate from './AddOrUpdate.vue'
 
 const links = ref([])
-
 const show = ref(false)
-
 const data = ref({})
-
 const isNew = ref(true)
 
 onMounted(async () => {
-  let linksCollection = await getDocs(collection(db, 'links'))
-  linksCollection.forEach((link) => {
-    links.value.push({ ...link.data(), id: link.id })
+  const linksCollection = collection(db, 'links')
+  onSnapshot(linksCollection, (snapshot) => {
+    links.value = snapshot.docs.map((doc) => ({ ...doc.data(), id: doc.id }))
   })
 })
 
-function toggleComponent(docData = {}, isNewDoc = true) {
-  data.value = docData
-  isNew.value = isNewDoc
+function toggleComponent(linkData = {}, isNewLink = true) {
+  data.value = linkData
+  isNew.value = isNewLink
   show.value = !show.value
 }
 
-async function deleteRecord(id) {
+async function deleteLink(id) {
   await deleteDoc(doc(db, 'links', id))
 }
 </script>
 
 <template>
   <div>
-    <div class="container my-4 text-center">
-      <h1 class="display-4 text-primary font-weight-bold text-uppercase" style="font-size: 2.5rem">
-        Sportler redirects
-      </h1>
-    </div>
-
+    <h1 class="text-center text-primary my-4">Sportler Redirects</h1>
     <div v-if="show" class="modal-overlay">
       <div class="modal-wrapper">
         <add-or-update @close="toggleComponent" :data="data" :isNew="isNew"></add-or-update>
       </div>
     </div>
 
-    <button class="btn btn-primary mb-3" @click="toggleComponent()">Add New Link</button>
+    <button class="btn btn-primary mb-3" @click="toggleComponent()">Add New</button>
     <div
       v-for="link in links"
       :key="link.id"
@@ -54,7 +46,7 @@ async function deleteRecord(id) {
       <div class="card-body">
         <h5 class="card-title">{{ link.name }}</h5>
         <p class="card-text">{{ link.link }}</p>
-        <button class="btn btn-danger" @click.stop="deleteRecord(link.id)">Delete</button>
+        <button class="btn btn-danger" @click.stop="deleteLink(link.id)">Delete</button>
       </div>
     </div>
   </div>
